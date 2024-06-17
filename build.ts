fe3a18db473c5ff2faf5cc85836db5cbc89ed540
fzn0x <fncolon@pm.me>
2024-06-18 06:19:38 +0700

    chore(build): fix webpack

--- build.ts
+++ build.ts
@@ -5,92 +5,76 @@
   Copyright (c) 2022 Taishi Naritomi
 */
 
-import { exec } from "child_process";
-import fs from "fs";
-import path from "path";
-import arg from "arg";
-import { build } from "esbuild";
-import type { Plugin, PluginBuild, BuildOptions } from "esbuild";
-import glob from "glob";
+import { exec } from 'child_process'
+import fs from 'fs'
+import path from 'path'
+import arg from 'arg'
+import { build } from 'esbuild'
+import type { Plugin, PluginBuild, BuildOptions } from 'esbuild'
+import { glob } from 'glob'
 
 const args = arg({
-  "--watch": Boolean,
-});
+  '--watch': Boolean,
+})
 
-const isWatch = args["--watch"] || false;
+const isWatch = args['--watch'] || false
 
-const entryPoints = glob.sync("./src/**/*.ts", {
-  ignore: [
-    "./src/**/*.test.ts",
-    "./src/mod.ts",
-    "./src/middleware.ts",
-    "./src/deno/**/*.ts",
-  ],
-});
+const entryPoints = glob.sync('./src/**/*.ts', {
+  ignore: ['./src/**/*.test.ts', './src/mod.ts', './src/deno/**/*.ts'],
+})
 
 /*
   This plugin is inspired by the following.
   https://github.com/evanw/esbuild/issues/622#issuecomment-769462611
 */
-const addExtension = (
-  extension: string = ".js",
-  fileExtension: string = ".ts"
-): Plugin => ({
-  name: "add-extension",
+const addExtension = (extension: string = '.js', fileExtension: string = '.ts'): Plugin => ({
+  name: 'add-extension',
   setup(build: PluginBuild) {
     build.onResolve({ filter: /.*/ }, (args) => {
       if (args.importer) {
-        const p = path.join(args.resolveDir, args.path);
-        let tsPath = `${p}${fileExtension}`;
+        const p = path.join(args.resolveDir, args.path)
+        let tsPath = `${p}${fileExtension}`
 
-        let importPath = "";
+        let importPath = ''
         if (fs.existsSync(tsPath)) {
-          importPath = args.path + extension;
+          importPath = args.path + extension
         } else {
-          tsPath = path.join(
-            args.resolveDir,
-            args.path,
-            `index${fileExtension}`
-          );
+          tsPath = path.join(args.resolveDir, args.path, `index${fileExtension}`)
           if (fs.existsSync(tsPath)) {
-            importPath = `${args.path}/index${extension}`;
+            importPath = `${args.path}/index${extension}`
           }
         }
-        return { path: importPath, external: true };
+        return { path: importPath, external: true }
       }
-    });
+    })
   },
-});
+})
 
 const commonOptions: BuildOptions = {
   watch: isWatch,
   entryPoints,
-  logLevel: "info",
-  platform: "node",
-};
+  logLevel: 'info',
+  platform: 'node',
+}
 
 const cjsBuild = () =>
   build({
     ...commonOptions,
-    outbase: "./src",
-    outdir: "./dist/cjs",
-    format: "cjs",
-  });
+    outbase: './src',
+    outdir: './dist/cjs',
+    format: 'cjs',
+  })
 
 const esmBuild = () =>
   build({
     ...commonOptions,
     bundle: true,
-    outbase: "./src",
-    outdir: "./dist",
-    format: "esm",
-    plugins: [addExtension(".js")],
-  });
+    outbase: './src',
+    outdir: './dist',
+    format: 'esm',
+    plugins: [addExtension('.js')],
+  })
 
-Promise.all([esmBuild(), cjsBuild()]);
+Promise.all([esmBuild(), cjsBuild()])
 
-exec(
-  `tsc ${
-    isWatch ? "-w" : ""
-  } --emitDeclarationOnly --declaration --project tsconfig.build.json`
-);
+exec(`tsc ${isWatch ? '-w' : ''} --emitDeclarationOnly --declaration --project tsconfig.build.json`)
